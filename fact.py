7770fc379f5d33223d2a5554cc9472ca7fbac127
prasadratnaparkhi <116976622+prasadratnaparkhi@users.noreply.github.com>
2024-04-26 16:47:25 +0530

    fact.py

---
+++ fact.py
@@ -0,0 +1,11 @@
+def factorial(x):
+    if x == 1:
+        return 1
+    else:
+        # recursive call to the function
+        return (x * factorial(x-1))
+# to take input from the user
+num = int(input("Enter a number: "))
+# call the factorial function
+result = factorial(num)
+print("The factorial of", num, "is", result)
